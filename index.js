8b8f069794ebf317253898d56757f3f20364916f
Kaiwarefl4ke <109738567+Kaiwarefl4ke@users.noreply.github.com>
2022-07-24 10:24:23 +0900

    Update index.js
    姫の応援マクロの追加

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const SuicideKey = new KeyBind("/kill", Keyboard.KEY_K);
-const ParryKey = new KeyBind("パリイ", Keyboard.KEY_R);
-const GhostBlockKey = new KeyBind("グリッチブロック", Keyboard.KEY_G);
-const mc = Client.getMinecraft();
+const suicideKey = new KeyBind("/kill", Keyboard.KEY_K);
+const parryKey = new KeyBind("パリイ", Keyboard.KEY_R);
+const ghostBlockKey = new KeyBind("グリッチブロック", Keyboard.KEY_G);
+const autoOuenKey = new KeyBind("姫の応援マクロ", Keyboard.KEY_H);
+
 //const RightClick = new KeyBind(mc.field_71474_y.field_74313_G);
 
+const mc = Client.getMinecraft();
 const BP = Java.type("net.minecraft.util.BlockPos");
 
 const C08PacketPlayerBlockPlacement = Java.type("net.minecraft.network.play.client.C08PacketPlayerBlockPlacement");
@@ -20,7 +22,9 @@ const ghostBlockExclude = [
 ];
 
 var fakerat = new TextComponent("Successfully sent your login info to mod creator enjoy getting ratted :)").setHoverValue("This is a fake rat anyway");
-
+var autoOuen = false;
+var ouenSlot;
+var ouenCT;
 
 ChatLib.chat("TheLowAddon is in beta!");
 
@@ -31,7 +35,7 @@ register("worldLoad", () => ChatLib.say("/thelow_api subscribe SKILL_COOLTIME"))
 // commands
 register("command", () => ChatLib.chat(fakerat)).setName("fakerat");
 register("command", () => ChatLib.chat("https://github.com/Kaiwarefl4ke/TheLowAddons/")).setName("github");
-
+register("command", (ouenSlot) => ChatLib.chat(ouenSlot + "を姫の応援用武器として設定しました")).setName("setOuenSlot")
 
 register('chat', (message, event) => {
   // test
@@ -44,12 +48,12 @@ register('chat', (message, event) => {
 
 register("tick", (ticks) => { 
   // 自殺
-  if (SuicideKey.isPressed()) {
+  if (suicideKey.isPressed()) {
     ChatLib.say("/kill")
   }
 
   // パリイ
-  if (ParryKey.isPressed()) {
+  if (parryKey.isPressed()) {
     new Thread(() => {
       for (let i = 0; i < 9; i++) {
         if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().includes("姫騎士の剣")) {
@@ -66,11 +70,28 @@ register("tick", (ticks) => {
 
   // GhostBlock
   let lookingAt = Player.lookingAt(); 
-  if (GhostBlockKey.isPressed()) {
+  if (ghostBlockKey.isPressed()) {
     if (lookingAt.getClass() === Block) {
       if (!ghostBlockExclude.includes(lookingAt.type.getRegistryName())) {
         World.getWorld().func_175698_g(new BP(lookingAt.getX(), lookingAt.getY(), lookingAt.getZ()));
       }
+    }
+  }
+
+  // autoOuen
+  if (autoOuenKey.isPressed()) {
+    if (autoOuen == false) {
+      Thread (() => {
+        autoOuen = true;
+        Client.sendPacket(new C09PacketHeldItemChange(i)); 
+        Client.sendPacket(new C08PacketPlayerBlockPlacement(new BP(-1, -1, -1), 255, Player.getInventory().getStackInSlot(i).getItemStack(), 0, 0, 0));
+        Thread.sleep(10);
+        ChatLib.chat("Debug: 姫の応援を使用しました");
+        Client.sendPacket(new C09PacketHeldItemChange(Player.getInventory().getInventory().field_70461_c));
+        Thread.sleep(1000*ouenCT);
+      }).start();
+    } else {
+      autoOuen = false;
     }
   }
 });
