e58d678c53dfa3bb0274d81dd3b0bb19c5804f0c
Kaiwarefl4ke <109738567+Kaiwarefl4ke@users.noreply.github.com>
2022-10-10 19:20:04 +0900

    Added some debug feature

--- index.js
+++ index.js
@@ -1,71 +1,52 @@
-const suicideKey = new KeyBind("/kill", Keyboard.KEY_K);
-const parryKey = new KeyBind("パリイ", Keyboard.KEY_F);
-const autoOuenKey = new KeyBind("姫の応援マクロ", Keyboard.KEY_H);
+const autoSniper = new KeyBind("AutoSniper", Keyboard.KEY_L);
+var sniper = false;
+ChatLib.chat("Debug: Stage 1 has been loaded");
 
-const mc = Client.getMinecraft();
-const BP = Java.type("net.minecraft.util.BlockPos");
-
-const C08PacketPlayerBlockPlacement = Java.type("net.minecraft.network.play.client.C08PacketPlayerBlockPlacement");
-const C09PacketHeldItemChange = Java.type("net.minecraft.network.play.client.C09PacketHeldItemChange");
-const C0APacketAnimation = Java.type("net.minecraft.network.play.client.C0APacketAnimation");
-
-var autoOuen = false;
-var ouenSlot;
-var ouenCT;
-
-
-register("worldLoad", () => ChatLib.say("/thelow_api subscribe SKILL_COOLTIME"));
-
-
-// commands
-register("command", () => ChatLib.chat("https://github.com/Kaiwarefl4ke/TheLowAddons/")).setName("github");
-
-
-register("tick", (ticks) => { 
-  // 自殺
-  if (suicideKey.isPressed()) {
-    ChatLib.say("/kill");
+register("tick", (ticks) => {
+  if (autoSniper.isPressed() && sniper === false) {
+    sniper = true;
+    ChatLib.chat("[Sniper]: AutoSniper: On");
+  } else {
+  	sniper = false;
+    ChatLib.chat("[Sniper]: AutoSniper: Off");
   }
-  // パリイ
-  if (parryKey.isPressed()) {
-    new Thread(() => {
-      for (let i = 0; i < 9; i++) {
-        if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().includes("姫騎士の剣")) {
-          Client.sendPacket(new C09PacketHeldItemChange(i));
-          Client.sendPacket(new C08PacketPlayerBlockPlacement(new BP(-1, -1, -1), 255, Player.getInventory().getStackInSlot(i).getItemStack(), 0, 0, 0));
-          //add the line below if you are laggy
-	  //Thread.sleep(1);
-          //ChatLib.chat("Debug: Parried!")
-          Client.sendPacket(new C09PacketHeldItemChange(Player.getInventory().getInventory().field_70461_c));
-          break;          
-        }
-      }
-    }).start();
-  }
-  // autoOuen
-  if (autoOuenKey.isPressed()) {
-    if (autoOuen === false) {
-      autoOuen = true;
-      new Thread(() => {
-        for (let i = 0; i < 9; i++) {
-          if (Player.getInventory().getStackInSlot(i) !== null && Player.getInventory().getStackInSlot(i).getName().includes(OuenWeapon)) {
-          Client.sendPacket(new C09PacketHeldItemChange(i));
-          Client.sendPacket(new C08PacketPlayerBlockPlacement(new BP(-1, -1, -1), 255, Player.getInventory().getStackInSlot(i).getItemStack(), 0, 0, 0));
-          Client.sendPacket(new C09PacketHeldItemChange(Player.getInventory().getInventory().field_70461_c));
-          break;          
-          }
-        }
-      }).start();
-    } else {
-      autoOuen = false;
-    }
-  }
+  try {
+  	if (sniper === true) {
+  		let playerList = getAllPlayers();
+  		new Thread(() => {
+    		if (playerList.toLowerCase().includes(snipeTarget)) {
+      		sniper = false;
+      		ChatLib.chat("[Sniper]: AutoSniper: Successfully sniped the target");
+	    	} else {
+  	  	  Thread.sleep(1000);
+    	  	if (snipeMode === "solos") {
+      			ChatLib.say("/play BEDWARS_EIGHT_ONE");
+      		} else if (snipeMode === "doubles") {
+        		ChatLib.say("/play BEDWARS_EIGHT_TWO");
+      		} else if (snipeMode === "3s") {
+        		ChatLib.say("/play BEDWARS_FOUR_THREE");
+      		} else if (snipeMode === "4s") {
+        		ChatLib.say("/play BEDWARS_FOUR_FOUR");
+      		}
+    		}
+  		});
+		}
+   } catch(e) {
+   	ChatLib.chat("[Sniper]: [Debug]: An internal error has occured on AutoSnipe module.");
+   }
 });
-	
-register('chat', (message, event) => {
-  // Skill Timer
-  if (message.toLowerCase().includes('apiType')) {
-    let API = JSON.parse(message);
-    ChatLib.chat(API);
+ChatLib.chat("Debug: Stage 2 has been loaded");
+
+register("command", (snipeTarget) => ChatLib.chat("The Target is set to" + snipeTarget + ".")).setName("sniper settarget");
+register("command", (snipeMode) => {
+  if (snipeMode === "help") {
+  	ChatLib.chat("Usage: /sniper setmode <solos/doubles/3s/4s/help>");
+  } else {
+	  ChatLib.chat("The Mode is set to" + snipeMode +".");
   }
-}).setCriteria("${message}");
+}).setName("sniper setmode");
+register("command", () => {
+  ChatLib.chat("/sniper settarget <playerName>");
+	ChatLib.chat("/sniper setmode <solos/doubles/3s/4s/help>");
+}).setName("sniper help");
+ChatLib.chat("Debug: All stages have been loaded successfully, GG!");
